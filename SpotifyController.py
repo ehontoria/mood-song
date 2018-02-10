df7b4399c1606b03586264834f79dc758d1a72f9
Joe <gabay.j@husky.neu.edu>
2018-02-10 14:19:45 -0500

    framework
    added framework

---
+++ SpotifyController.py
@@ -0,0 +1,26 @@
+import spotipy
+import spotipy.util as util
+
+
+#Auth information - super duper top secret 'kay?
+SPOTIPY_CLIENT_ID="1dd9a22efd17418da5db84567d91208e"
+SPOTIPY_CLIENT_SECRET="2efe887783454bae8aa67492d33b4ddc"
+SPOTIPY_REDIRECT_URI="http://localhost/"
+
+scopeType = 'user-modify-playback-state,user-read-currently-playing'
+#token = "AQAZf1wmSO80gorJ4PnntZZ2qNrIKFjuJXyvTxyE6BMZJzXu4vB2ZIWs0uT4Ww2AEQK0zKnUZpFIwjjxpjdNSHBffAUCiD9Z-S50kveCbV-862ON836FF9iuyZzu0juJxcrmKt1noaW5co3d3WDYL3zZshsS88oScWQJCDNNM8Rypv-uFTTp"
+
+class SpotifyController:
+    def __init__(self, username="electriczap4"):
+        token = util.prompt_for_user_token(username, client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI,scope = scopeType)
+        self.spot = spotipy.Spotify(auth=token)
+        
+    def pause(self):
+        self.spot.pause_playback()
+
+    def getC(self):
+        return self.spot.currently_playing()
+        
+
+spotcontr = SpotifyController()
+cur = spotcontr.getC()
