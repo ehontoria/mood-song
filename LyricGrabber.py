914a8000ab290a55069bfa000ab0038efcf70dc1
Joe <gabay.j@husky.neu.edu>
2018-02-10 16:18:45 -0500

    added lyric scraper

--- LyricGrabber.py
+++ LyricGrabber.py
@@ -7,11 +7,9 @@ apiroot='http://api.musixmatch.com/ws/1.1/'
 
 grabber='track.lyrics.get?track_id='
 
-apikey = "&apikey=c00142a8f220230892ebcc53784db989"
+mmapikey = "&apikey=c00142a8f220230892ebcc53784db989"
 
 testid='15953433'
-
-res = requests.get(apiroot + grabber + testid + apikey)
 
 
 
@@ -19,5 +17,27 @@ data = json.loads(res.content,encoding="UTF-8")
 
 
 
-#def getLyrics(songID):
+def getLyrics(songID):
+    res = requests.get(apiroot + grabber + songID + mmapikey)
+    data = json.loads(res.content,encoding="UTF-8")
+
+    #Returns lyrics, separated with newline escapes. Disclaimer needs to be stripped.
+    lyrics=data['message']['body']['lyrics']['lyrics_body']
+
+    #Strip disclaimer and extra data:
+    if lyrics.endswith('\n\n...\n\n******* This Lyrics is NOT for Commercial use *******'):
+        return lyrics = lyrics[:len('\n\n...\n\n******* This Lyrics is NOT for Commercial use *******')]
+
+    else:
+        #panic - aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        return 0
+
+def getScore(lyrics)
+
+    
+
+    
+    
+    
     
